2d1779532f788c882bbbd53faef94ff3b7f689b7
Kangili <kangilimabonda29@gmail.com>
2025-07-28 20:29:22 +0200

    mise à jour du projet dans About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,10 +9,10 @@ function About (){
     <>
       <ImageBanner/>
       <div className="about_container"> 
-         <DescriptionPanel title = "Fiabilité" content ="C'est très important"/>
-         <DescriptionPanel title = "Respect" content = "C'est très important"/>
-         <DescriptionPanel title = "Service" content = "C'est très important"/>
-         <DescriptionPanel title = "Sécurité" content = "C'est très important"/>
+         <DescriptionPanel title = "Fiabilité" content ="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."/>
+         <DescriptionPanel title = "Respect" content = "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de pertubation du voisinage entraînera une exclusion de notre plateforme."/>
+         <DescriptionPanel title = "Service" content = "La qualité du service est au coeur de notre engagement chez Kasa. Nous veillions à ce que chaque interaction, que ce soit avec nos hôtes ou nos locataires, soit empreinte de respect et de bienveillance."/>
+         <DescriptionPanel title = "Sécurité" content = "La sécruité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant un note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que des standards sont bien respectés. Nous organisons également des atliers sur la sécurité domestique pour nos hôtes."/>
       </div>
     </>
   );
